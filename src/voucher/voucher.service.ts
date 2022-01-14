133556c4f77eb33840ea74481b5c5961cac19b2e
pandu <pandu@softwareseni.com>
2022-01-14 13:02:13 +0700

    feat(voucher): added redeem voucher

--- src/voucher/voucher.service.ts
+++ src/voucher/voucher.service.ts
@@ -1,3 +1,4 @@
+import { VoucherCodesRepository } from './../voucher_code/repository/voucher_code.repository';
 import { VouchersRepository } from './repository/voucher.repository';
 import {
   BadRequestException,
@@ -11,8 +12,11 @@ import { ResponseService } from 'src/response/response.service';
 import {
   DiscountTypeVoucherEnum,
   OrderTypeVoucherEnum,
+  StatusVoucherEnum,
   VoucherDocument,
 } from './entities/voucher.entity';
+import moment from 'moment';
+import { StatusVoucherCodeGroup } from 'src/voucher_code/entities/voucher_code.entity';
 
 @Injectable()
 export class VoucherService {
@@ -20,6 +24,7 @@ export class VoucherService {
     private readonly messageService: MessageService,
     private readonly responseService: ResponseService,
     private readonly vouchersRepository: VouchersRepository,
+    private readonly voucherCodesRepository: VoucherCodesRepository,
   ) {}
   private readonly logger = new Logger(VoucherService.name);
 
@@ -59,6 +64,222 @@ export class VoucherService {
       });
     } catch (error) {
       this.logger.log(error);
+      throw new BadRequestException(
+        this.responseService.error(
+          HttpStatus.BAD_REQUEST,
+          {
+            value: '',
+            property: '',
+            constraint: [
+              this.messageService.get('general.list.fail'),
+              error.message,
+            ],
+          },
+          'Bad Request',
+        ),
+      );
+    }
+  }
+
+  getDurationInt(duration) {
+    let days = 0;
+    switch (duration) {
+      case '1DAY':
+        days = 1;
+        break;
+
+      case '3DAYS':
+        days = 3;
+        break;
+
+      case '7DAYS':
+        days = 7;
+        break;
+
+      case '10DAYS':
+        days = 10;
+        break;
+
+      case '14DAYS':
+        days = 14;
+        break;
+
+      case '30DAYS':
+        days = 30;
+        break;
+    }
+    return days;
+  }
+
+  async redeemVoucher(data, customer_id) {
+    // NOT AUTO GENERATE
+    let voucherCode = await this.voucherCodesRepository.findOne({
+      where: { code: data.code, status: StatusVoucherCodeGroup.ACTIVE },
+      relations: ['master_vouchers'],
+    });
+
+    if (voucherCode) {
+      if (voucherCode.quota) {
+        const count = await this.vouchersRepository.count({
+          where: {
+            voucher_code_id: voucherCode.id,
+            status: StatusVoucherEnum.ACTIVE,
+          },
+        });
+        console.log(voucherCode);
+
+        // if (count < voucherCode.quota) {
+        const postVoucherDatas = [];
+        for (let i = 0; i < voucherCode.master_vouchers.length; i++) {
+          const master_voucher = voucherCode.master_vouchers[i];
+          const date_start = new Date();
+          const days = this.getDurationInt(master_voucher.duration);
+          const date_end = moment(date_start).add(days, 'days');
+          const postVoucherData = {
+            voucher_code_id: voucherCode.id,
+            customer_id,
+            code:
+              voucherCode.code + Math.floor(Math.random() * (100 - 1 + 1)) + 1,
+            type: master_voucher.type,
+            order_type: master_voucher.order_type,
+            target: voucherCode.target,
+            status: StatusVoucherEnum.ACTIVE,
+            date_start,
+            date_end,
+            minimum_transaction: master_voucher.minimum_transaction,
+            discount_type: master_voucher.discount_type,
+            discount_value: master_voucher.discount_value,
+            discount_maximum: master_voucher.discount_maximum,
+            is_combinable: master_voucher.is_combinable,
+          };
+          postVoucherDatas.push(postVoucherData);
+        }
+        await this.createVoucherBulk(postVoucherDatas);
+        // } else {
+        //   throw new BadRequestException(
+        //     this.responseService.error(
+        //       HttpStatus.BAD_REQUEST,
+        //       {
+        //         value: `${voucherCode.quota}`,
+        //         property: 'quota',
+        //         constraint: [
+        //           this.messageService.get('general.create.fail'),
+        //           'quota full',
+        //         ],
+        //       },
+        //       'Bad Request',
+        //     ),
+        //   );
+        // }
+      } else {
+        const postVoucherDatas = [];
+        for (let i = 0; i < voucherCode.master_vouchers.length; i++) {
+          const master_voucher = voucherCode.master_vouchers[i];
+          const date_start = new Date();
+          const days = this.getDurationInt(master_voucher.duration);
+          const date_end = moment(date_start).add(days, 'days');
+          const postVoucherData = {
+            voucher_code_id: voucherCode.id,
+            customer_id,
+            code: voucherCode.code,
+            type: master_voucher.type,
+            order_type: master_voucher.order_type,
+            target: voucherCode.target,
+            status: StatusVoucherEnum.ACTIVE,
+            date_start,
+            date_end,
+            minimum_transaction: master_voucher.minimum_transaction,
+            discount_type: master_voucher.discount_type,
+            discount_value: master_voucher.discount_value,
+            discount_maximum: master_voucher.discount_maximum,
+            is_combinable: master_voucher.is_combinable,
+          };
+          postVoucherDatas.push(postVoucherData);
+        }
+      }
+    } else {
+      // AUTO GENERATE
+      const vouchers = await this.vouchersRepository.find({
+        where: { code: data.code, customer_id: null },
+      });
+
+      if (vouchers.length > 0) {
+        const voucher = vouchers[0];
+        voucher.status = StatusVoucherEnum.ACTIVE;
+        const date_start = new Date();
+        const days = this.getDurationInt(master_voucher.duration);
+
+        // voucherCode = await this.voucherCodesRepository.findOne({
+        //   where: { id: voucher.voucher_code_id },
+        //   relations: ['master_vouchers'],
+        // });
+        // const postVoucherDatas = [];
+        // for (let i = 0; i < voucherCode.master_vouchers.length; i++) {
+        //   const master_voucher = voucherCode.master_vouchers[i];
+        //   const date_start = new Date();
+        //   const days = this.getDurationInt(master_voucher.duration);
+        //   const date_end = moment(date_start).add(days, 'days');
+        //   const postVoucherData = {
+        //     voucher_code_id: voucherCode.id,
+        //     customer_id,
+        //     code: voucherCode.code,
+        //     type: master_voucher.type,
+        //     order_type: master_voucher.order_type,
+        //     target: voucherCode.target,
+        //     status: StatusVoucherEnum.ACTIVE,
+        //     date_start,
+        //     date_end,
+        //     minimum_transaction: master_voucher.minimum_transaction,
+        //     discount_type: master_voucher.discount_type,
+        //     discount_value: master_voucher.discount_value,
+        //     discount_maximum: master_voucher.discount_maximum,
+        //     is_combinable: master_voucher.is_combinable,
+        //   };
+        //   postVoucherDatas.push(postVoucherData);
+        // }
+        // await this.createVoucherBulk(postVoucherDatas);
+      } else {
+        throw new BadRequestException(
+          this.responseService.error(
+            HttpStatus.BAD_REQUEST,
+            {
+              value: `${voucherCode.quota}`,
+              property: 'quota',
+              constraint: [
+                this.messageService.get('general.create.fail'),
+                'quota full',
+              ],
+            },
+            'Bad Request',
+          ),
+        );
+      }
+    }
+
+    return {};
+  }
+
+  async getMyVouchers(data, customer_id) {
+    try {
+      const page = data.page || 1;
+      const limit = data.limit || 10;
+      const offset = (page - 1) * limit;
+
+      const [items, count] = await this.vouchersRepository.findAndCount({
+        take: limit,
+        skip: offset,
+        where: { customer_id, status: StatusVoucherEnum.ACTIVE },
+      });
+
+      const listItems = {
+        current_page: parseInt(page),
+        total_item: count,
+        limit: parseInt(limit),
+        items: items,
+      };
+
+      return listItems;
+    } catch (error) {
       throw new BadRequestException(
         this.responseService.error(
           HttpStatus.BAD_REQUEST,
