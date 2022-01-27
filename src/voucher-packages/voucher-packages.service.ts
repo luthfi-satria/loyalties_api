ce76bc5ccf751fe51dedf22cd2bd3ce096f07a4e
okipratama97 <okipratama97@gmail.com>
2022-01-27 10:49:33 +0700

    update: list voucher packages

--- src/voucher-packages/voucher-packages.service.ts
+++ src/voucher-packages/voucher-packages.service.ts
@@ -178,7 +178,11 @@ export class VoucherPackagesService {
         where = { ...where, price: LessThanOrEqual(params.price_max) };
       }
 
-      const query = this.mainQuery().where(where).take(limit).skip(offset);
+      const query = this.mainQuery()
+        .where(where)
+        .orderBy('created_at', 'DESC')
+        .take(limit)
+        .skip(offset);
 
       // const items = await query.getMany();
       const count = await query.getCount();
